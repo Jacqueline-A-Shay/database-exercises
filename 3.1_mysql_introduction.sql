f3a332ae5fb9ffc3ea64c84e6aa83d1ccbcaf1f4
Jacqueline Shay <Jacqueline.A.Shay@gmail.com>
2019-09-03 14:18:51 -0500

    first sql query

---
+++ 3.1_mysql_introduction.sql
@@ -0,0 +1,9 @@
+SHOW databases;
+-- mysql. user means use the mysql database and read from the user table
+SELECT * FROM mysql.user;
+-- select the user and host columns from the user table
+SELECT user, host FROM mysql.user;
+-- select all columns from the help_topic table on the mysql database
+SELECT * FROM mysql.help_topic;
+-- SELECT only certain columns
+SELECT help_topic_id, help_category_id, url FROM mysql.help_topic;
