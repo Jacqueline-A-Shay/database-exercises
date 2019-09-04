46a35240187548980347d5beb962c94df6ac4714
Jacqueline Shay <Jacqueline.A.Shay@gmail.com>
2019-09-04 11:08:20 -0500

    add 3.5.2

---
+++ 3.5.2_order_by_exercises.sql
@@ -0,0 +1,11 @@
+use employees;
+
+select * from employees where first_name IN ('Irena', 'Vidya', 'Maya') order by first_name;
+select * from employees where first_name IN ('Irena', 'Vidya', 'Maya') order by first_name, last_name;
+select * from employees where last_name like ('%E%') order by emp_no;
+select * from employees where first_name IN ('Irena', 'Vidya', 'Maya') order by first_name DESC, last_name DESC;
+select * from employees where last_name like ('%E%') order by emp_no DESC;
+
+select * from employees 
+WHERE (hire_date between "1990-01-01" and "1999-12-31") AND birth_date like "%-12-25" 
+ORDER BY hire_date  DESC, birth_date ASC;
