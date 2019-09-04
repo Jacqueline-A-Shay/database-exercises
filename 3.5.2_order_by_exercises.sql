7ef39df211ae9aecbd3e2ee0ce8d2c8587196e4e
Jacqueline Shay <Jacqueline.A.Shay@gmail.com>
2019-09-04 11:38:03 -0500

    modify 3.5

--- 3.5.2_order_by_exercises.sql
+++ 3.5.2_order_by_exercises.sql
@@ -2,8 +2,11 @@ use employees;
 
 select * from employees where first_name IN ('Irena', 'Vidya', 'Maya') order by first_name;
 select * from employees where first_name IN ('Irena', 'Vidya', 'Maya') order by first_name, last_name;
-select * from employees where last_name like ('%E%') order by emp_no;
-select * from employees where first_name IN ('Irena', 'Vidya', 'Maya') order by first_name DESC, last_name DESC;
+select * from employees where last_name like ('%E') order by emp_no;
+select * from employees where last_name like ('E%') order by emp_no;
+
+select * from employees where first_name IN ('Irena', 'Vidya', 'Maya') 
+order by first_name DESC, last_name DESC;
 select * from employees where last_name like ('%E%') order by emp_no DESC;
 
 select * from employees 
