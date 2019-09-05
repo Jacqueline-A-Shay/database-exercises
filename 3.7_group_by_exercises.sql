6e5e5987f17099280fcbdb3a3c167c66cb6417a3
Jacqueline Shay <Jacqueline.A.Shay@gmail.com>
2019-09-05 09:42:25 -0500

    more touch  3.7

--- 3.7_group_by_exercises.sql
+++ 3.7_group_by_exercises.sql
@@ -9,6 +9,8 @@ concat(first_name, ' ', last_name) as full_name
 from employees where last_name like 'e%e'
 group by full_name;
 
+select first_name, last_name from employees.employees where last_name like 'e%e' group by last_name, first_name;
+
 select last_name from employees where (last_name like "%q%" ) AND (last_name not like "%qu%") group by last_name;
 
 select 
@@ -55,3 +57,8 @@ select count(*) as count_all, count(distinct lower(concat(
                 "_",
                 substr(birth_date, 6,2),
                 substr(birth_date,3,2)))))as difference  from employees;
+-- soln
+SELECT COUNT(*) FROM
+(SELECT COUNT(*) AS username_count, LOWER(CONCAT(SUBSTR(first_name,1,1),SUBSTR(last_name,1,4),'_',SUBSTR(birth_date,6,2),SUBSTR(birth_date,3,2))) AS username FROM employees.employees
+ GROUP BY username HAVING username_count>1) AS test;
+ 
