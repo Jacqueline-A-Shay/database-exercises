e930f68f9106e4517e965692fc8c5a22b4d57588
Jacqueline Shay <Jacqueline.A.Shay@gmail.com>
2019-09-09 12:12:18 -0500

    update  3.11

--- 3.11_more_exercises.sql
+++ 3.11_more_exercises.sql
@@ -135,3 +135,141 @@ SELECT Region, SUM(Population) FROM country GROUP BY Region ORDER BY SUM(Populat
 -- +---------------------------+------------+
 -- 25 rows in set (0.00 sec)
 
+-- What is the population for each continent?
+SELECT Continent, SUM(Population) FROM country GROUP BY Continent ORDER BY SUM(Population) DESC;
+-- +---------------+------------+
+-- | Continent     | population |
+-- +---------------+------------+
+-- | Asia          | 3705025700 |
+-- | Africa        |  784475000 |
+-- | Europe        |  730074600 |
+-- | North America |  482993000 |
+-- | South America |  345780000 |
+-- | Oceania       |   30401150 |
+-- | Antarctica    |          0 |
+-- +---------------+------------+
+-- 7 rows in set (0.00 sec)
+
+-- What is the average life expectancy globally?
+SELECT AVG(LifeExpectancy) FROM country;
+
+-- +---------------------+
+-- | avg(LifeExpectancy) |
+-- +---------------------+
+-- |            66.48604 |
+-- +---------------------+
+-- 1 row in set (0.00 sec)
+
+-- What is the average life expectancy for each region, each continent? Sort the results from shortest to longest
+SELECT Continent, AVG(LifeExpectancy) FROM country GROUP BY Continent ORDER BY AVG(LifeExpectancy);
+
+-- -- +---------------+-----------------+
+-- -- | Continent     | life_expectancy |
+-- -- +---------------+-----------------+
+-- -- | Antarctica    |            NULL |
+-- -- | Africa        |        52.57193 |
+-- -- | Asia          |        67.44118 |
+-- -- | Oceania       |        69.71500 |
+-- -- | South America |        70.94615 |
+-- -- | North America |        72.99189 |
+-- -- | Europe        |        75.14773 |
+-- -- +---------------+-----------------+
+-- 7 rows in set (0.00 sec)
+
+SELECT Region, AVG(LifeExpectancy) FROM country GROUP BY Region ORDER BY AVG(LifeExpectancy);
+-- +---------------------------+-----------------+
+-- | Region                    | life_expectancy |
+-- +---------------------------+-----------------+
+-- | Antarctica                |            NULL |
+-- | Micronesia/Caribbean      |            NULL |
+-- | Southern Africa           |        44.82000 |
+-- | Central Africa            |        50.31111 |
+-- | Eastern Africa            |        50.81053 |
+-- | Western Africa            |        52.74118 |
+-- | Southern and Central Asia |        61.35000 |
+-- | Southeast Asia            |        64.40000 |
+-- | Northern Africa           |        65.38571 |
+-- | Melanesia                 |        67.14000 |
+-- | Micronesia                |        68.08571 |
+-- | Baltic Countries          |        69.00000 |
+-- | Eastern Europe            |        69.93000 |
+-- | Middle East               |        70.56667 |
+-- | Polynesia                 |        70.73333 |
+-- | South America             |        70.94615 |
+-- | Central America           |        71.02500 |
+-- | Caribbean                 |        73.05833 |
+-- | Eastern Asia              |        75.25000 |
+-- | North America             |        75.82000 |
+-- | Southern Europe           |        76.52857 |
+-- | British Islands           |        77.25000 |
+-- | Western Europe            |        78.25556 |
+-- | Nordic Countries          |        78.33333 |
+-- | Australia and New Zealand |        78.80000 |
+-- +---------------------------+-----------------+
+-- 25 rows in set (0.00 sec)
+
+-- Find all the countries whose local name is different from the official name
+SELECT c.Name 'Countries Having Official Name Different From Local Name'
+FROM country c
+WHERE c.Name NOT IN (SELECT c.LocalName FROM country c);
+
+-- Countries Having Official Name Different From Local Name
+-- Afghanistan
+-- Albania
+-- Netherlands Antilles
+-- United Arab Emirates
+-- Armenia
+-- American Samoa
+-- Antarctica
+-- French Southern territories
+-- Austria
+-- Azerbaijan
+-- Burundi
+-- Belgium
+-- Bulgaria
+-- Bahrain
+-- Bahamas
+-- Bosnia and Herzegovina
+-- Brazil
+
+-- Total 135 rows
+
+-- How many countries have a life expectancy less than x?
+SELECT count(*) as ' The number of countries having average life expentancy less than Bulgaria' FROM country WHERE LifeExpectancy < (SELECT LifeExpectancy from country c where c.Name = 'Bulgaria');
+-- The number of countries having average life expentancy less than Bulgaria
+-- 117
+
+-- What state is city x located in?
+SELECT District FROM city c WHERE CountryCode = 'USA' AND Name = 'Clearwater';
+-- District
+-- Florida
+
+-- What region of the world is city x located in?
+
+SELECT Region 
+FROM city c 
+JOIN country cy ON c.CountryCode =cy.Code
+WHERE c.Name = 'Herat';
+
+-- Region
+-- Southern and Central Asia
+
+-- What country (use the human readable name) city x located in?
+
+SELECT cy.Name as 'Country where the city Herat belongs:'
+FROM city c 
+JOIN country cy ON c.CountryCode =cy.Code
+WHERE c.Name = 'Herat';
+-- Country where the city Herat belongs:
+-- Afghanistan
+
+
+-- What is the life expectancy in city x?
+SELECT cy.LifeExpectancy as 'How long do the residance of Herat, Afghanistan typically live? '
+FROM city c 
+JOIN country cy ON c.CountryCode =  cy.Code
+WHERE c.Name = 'Herat';
+select * from city;
+
+-- How long do the residance of Herat, Afghanistan typically live? 
+-- 45.9
