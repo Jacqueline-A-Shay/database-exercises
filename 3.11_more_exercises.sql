17c359f5e9c5744e70f2a7c7dd78e0bd047a414c
Jacqueline Shay <Jacqueline.A.Shay@gmail.com>
2019-09-09 18:41:22 -0500

    update  3.11

--- 3.11_more_exercises.sql
+++ 3.11_more_exercises.sql
@@ -376,3 +376,65 @@ GROUP BY c.name
 ORDER BY SUM(p.amount) DESC
 LIMIT 5;
 
+-- SELECT statements
+
+-- Select all columns from the actor table.
+SELECT * FROM actor;
+
+-- Select only the last_name column from the actor table.
+SELECT last_name FROM actor;
+
+-- Select only the following columns from the film table.
+SELECT title FROM film;
+
+/* DISTINCT operator */
+
+/* Select all distinct (different) last names from the actor table. */
+SELECT DISTINCT(last_name) FROM actor;
+/* 
+Select all distinct (different) postal codes from the address table. */
+SELECT DISTINCT(postal_code) FROM address;
+/* 
+Select all distinct (different) ratings from the film table. */
+SELECT DISTINCT(rating) FROM film;
+
+/* WHERE clause
+
+Select the title, description, rating, movie length columns from the films table that last 3 hours or longer. */
+SELECT title, description, rating, length FROM film WHERE length > 180;
+/* 
+Select the payment id, amount, and payment date columns from the payments table for payments made on or after 05/27/2005. */
+SELECT payment_id, amount, payment_date FROM payment WHERE payment_date > '2005-05-27 00:00:00' ORDER BY payment_date;
+/* 
+Select the primary key, amount, and payment date columns from the payment table for payments made on 05/27/2005. */
+SHOW CREATE TABLE payment;
+SELECT payment_id, amount, payment_date FROM payment WHERE DATE_FORMAT(payment_date, '%Y %M %D') = DATE_FORMAT('2005-05-27', '%Y %M %D') ;
+/* 
+Select all columns from the customer table for rows that have a last names beginning with S and a first names ending with N. */
+SELECT * FROM customer WHERE last_name LIKE 'S%' AND first_name LIKE 'N%';
+
+/* Select all columns from the customer table for rows where the customer is inactive or has a last name beginning with "M". */
+SELECT * FROM customer WHERE active = 0;
+/* 
+Select all columns from the category table for rows where the primary key is greater than 4 and the name field begins with either C, S or T. */
+SELECT * FROM category WHERE category_id > 4 AND name LIKE 'C%' OR name LIKE 'S%' OR name LIKE 'T%';
+
+/* Select all columns minus the password column from the staff table for rows that contain a password. */
+USE bayes_817;
+CREATE TABLE drop_pwd
+AS (SELECT * FROM sakila.staff WHERE password NOT LIKE ' ');
+select*from drop_pwd;
+ALTER TABLE drop_pwd DROP COLUMN password;
+select*from drop_pwd;
+
+/* Select all columns minus the password column from the staff table for rows that do not contain a password. */
+USE bayes_817;
+CREATE TABLE drop_pwd_null
+AS (SELECT * FROM sakila.staff WHERE password IS NULL);
+select*from drop_pwd_null;
+ALTER TABLE drop_pwd_null DROP COLUMN password;
+select*from drop_pwd_null;
+
+
+
+
