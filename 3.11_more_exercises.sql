e1c54455d954cd6946308fcb617deaa59affe84b
Jacqueline Shay <Jacqueline.A.Shay@gmail.com>
2019-09-09 20:46:35 -0500

    update  3.11

--- 3.11_more_exercises.sql
+++ 3.11_more_exercises.sql
@@ -435,6 +435,34 @@ select*from drop_pwd_null;
 ALTER TABLE drop_pwd_null DROP COLUMN password;
 select*from drop_pwd_null;
 
+/* IN operator
+USE sakila;
+Select the phone and district columns from the address table for addresses in California, England, Taipei, or West Java. */
+select* from address;
+SELECT a.phone, a.district FROM address a WHERE a.district IN ('England','Taipei', 'West Java');
+
+/* Select the payment id, amount, and payment date columns from the payment table for payments made on 05/25/2005, 05/27/2005, and 05/29/2005. (Use the IN operator and the DATE function, instead of the AND operator as in previous exercises.) */
+ SELECT payment_id, amount, payment_date FROM payment WHERE DATE(payment_date) IN ('2005-05-25', '2005-05-27','2005-05-29') ORDER BY payment_date;
+ 
+/* Select all columns from the film table for films rated G, PG-13 or NC-17. */
+SELECT * FROM film WHERE rating IN ('G','PG-13', 'NC-17');
 
 
+/* BETWEEN operator
 
+Select all columns from the payment table for payments made between midnight 05/25/2005 and 1 second before midnight 05/26/2005. */
+SELECT * FROM payment WHERE payment_date BETWEEN '2005-05-25 00:00:00' AND '2005-05-25 23:59:59' ORDER BY payment_date DESC;
+/* 
+Select the following columns from the film table for films where the length of the description is between 100 and 120.
+Hint: total_rental_cost = rental_duration * rental_rate */
+
+/* LIKE operator
+
+Select the following columns from the film table for rows where the description begins with "A Thoughtful". */
+SELECT * FROM film WHERE description LIKE 'A Thoughtful%';
+
+/* Select the following columns from the film table for rows where the description ends with the word "Boat". */
+SELECT * FROM film WHERE  description LIKE '%Boat';
+/* 
+Select the following columns from the film table where the description contains the word "Database" and the length of the film is greater than 3 hours. */
+SELECT * FROM film WHERE description LIKE '%Database%' AND length > 180;
